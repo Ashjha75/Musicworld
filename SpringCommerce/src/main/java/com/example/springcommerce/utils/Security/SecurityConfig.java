dd078042b31e228d008c4ada1cc43ed49c7fa839
Ashish jha <ajha5645@gmail.com>
2024-10-22 17:07:18 +0530

    this commit will add the AuthEntryPointJwt.java and updates of SecurityConfig.java

--- SpringCommerce/src/main/java/com/example/springcommerce/utils/Security/SecurityConfig.java
+++ SpringCommerce/src/main/java/com/example/springcommerce/utils/Security/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -18,5 +22,18 @@ public class SecurityConfig {
         http.sessionManagement((session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)));
         http.httpBasic((withDefaults()));
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userInfo = User.withUsername("user")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+        UserDetails adminInfo = User.withUsername("admin")
+                .password("{noop}password")
+                .roles("Admin")
+                .build();
+        return new InMemoryUserDetailsManager(userInfo, adminInfo);
     }
 }
