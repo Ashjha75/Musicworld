1e3f1378a99fed4ea1e4a794f37273b89e3a8179
Ashish jha <ajha5645@gmail.com>
2024-10-22 19:55:41 +0530

    this commit will add the LoginRequest.java and LoginResponse.java

--- SpringCommerce/src/main/java/com/example/springcommerce/utils/Security/LoginResponse.java
+++ SpringCommerce/src/main/java/com/example/springcommerce/utils/Security/LoginResponse.java
@@ -1,4 +1,18 @@
 package com.example.springcommerce.utils.Security;
 
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class LoginResponse {
+    private String jwtToken;
+
+    private String userName;
+    private List<String> roles;
+
 }
