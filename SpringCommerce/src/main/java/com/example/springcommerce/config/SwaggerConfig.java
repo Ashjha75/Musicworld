a08b4db8da8597d85f5affe72c5a026d42bf2597
Ashish jha <ajha5645@gmail.com>
2024-11-12 23:58:13 +0530

    This commit will add new tag manual ordering

--- SpringCommerce/src/main/java/com/example/springcommerce/config/SwaggerConfig.java
+++ SpringCommerce/src/main/java/com/example/springcommerce/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ package com.example.springcommerce.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,15 +16,18 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI().info(
-                new Info()
-                        .title("Spring Commerce API")
-                        .version("1.0")
-                        .description("API for Spring Commerce")
-        )
+                        new Info()
+                                .title("Spring Commerce API")
+                                .version("1.0")
+                                .description("API for Spring Commerce")
+                )
                 .servers(List.of(new Server().url("http://localhost:8000").description("Local server")
                         , new Server().url("https://spring-commerce.herokuapp.com").description("Heroku server")
                 ))
-
-                ;
+                .tags(List.of(new Tag().name("Authentication API")
+                        , new Tag().name("Category API")
+                        , new Tag().name("Cart API")
+                        , new Tag().name("Product API")
+                ));
     }
 }
