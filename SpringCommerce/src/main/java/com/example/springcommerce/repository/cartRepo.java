be827c090d7f91a9c23671c16056106be6939fe7
Ashish jha <ajha5645@gmail.com>
2024-12-03 20:46:30 +0530

    this will add the new cart Repo query

--- SpringCommerce/src/main/java/com/example/springcommerce/repository/cartRepo.java
+++ SpringCommerce/src/main/java/com/example/springcommerce/repository/cartRepo.java
@@ -2,8 +2,12 @@ package com.example.springcommerce.repository;
 
 import com.example.springcommerce.entity.cartEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface cartRepo extends JpaRepository<cartEntity, Long> {
+
+    @Query("SELECT c FROM cartEntity c WHERE c.user.email = ?1")
+     cartEntity findCartByEmail(String email);
 }
