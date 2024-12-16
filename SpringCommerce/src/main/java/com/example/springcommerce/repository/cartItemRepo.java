a8dd8fd24563c08778b912072c544b4ba8815be7
Ashish jha <ajha5645@gmail.com>
2024-12-16 23:15:13 +0530

    this will add the new cartitemquerry in cartItemRepo.java

--- SpringCommerce/src/main/java/com/example/springcommerce/repository/cartItemRepo.java
+++ SpringCommerce/src/main/java/com/example/springcommerce/repository/cartItemRepo.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface cartItemRepo extends JpaRepository<cartItemsEntity, Long> {
+    @query("SELECT ci FROM carts ci WHERE ci.cart.id = ?1 AND ci.product.id = ?2")
+    cartItemsEntity findCartItemByProductIDAndCartId(Long cartId, Long productId);
+
 }
