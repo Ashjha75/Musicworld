13abf758e47dc5dc096f61e8f892bf219e7ff593
Ashish jha <ajha5645@gmail.com>
2024-12-21 23:52:12 +0530

    this commit will add new endpoint to getAllCarts

--- SpringCommerce/src/main/java/com/example/springcommerce/service/cartService.java
+++ SpringCommerce/src/main/java/com/example/springcommerce/service/cartService.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface cartService {
     cartRequest addProductTocart(Long productId, Integer quantity);
     List<cartRequest> getAllCarts();
+    cartRequest getCart(String email, Long cartId);
 }
