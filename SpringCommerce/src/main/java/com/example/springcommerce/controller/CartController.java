f39bcfa2431e9c5963661f9e1b7035e59432447a
Ashish jha <ajha5645@gmail.com>
2024-12-21 23:52:02 +0530

    this commit will add new endpoint to getAllCarts

--- SpringCommerce/src/main/java/com/example/springcommerce/controller/CartController.java
+++ SpringCommerce/src/main/java/com/example/springcommerce/controller/CartController.java
@@ -1,7 +1,10 @@
 package com.example.springcommerce.controller;
 
 import com.example.springcommerce.DTO.Request.cartRequest;
+import com.example.springcommerce.entity.cartEntity;
+import com.example.springcommerce.repository.cartRepo;
 import com.example.springcommerce.service.cartService;
+import com.example.springcommerce.utils.utilityGroup.AuthUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +20,14 @@ import java.util.List;
 public class CartController {
 
     private final cartService cartService;
+    private final AuthUtil authUtil;
+    private final com.example.springcommerce.repository.cartRepo cartRepo;
 
     @Autowired
-    public CartController(cartService cartService) {
+    public CartController(cartService cartService, AuthUtil authUtil, cartRepo cartRepo) {
         this.cartService = cartService;
+        this.authUtil = authUtil;
+        this.cartRepo = cartRepo;
     }
 
     @PostMapping("/product/{productId}/quantity/{quantity}")
@@ -40,4 +47,16 @@ public class CartController {
 
         return new ResponseEntity<List<cartRequest>>(cartRequest, HttpStatus.FOUND);
     }
+
+    @GetMapping("/get-cart")
+    @Operation(summary = "Get cart by ID", description = "Get cart by ID")
+    public ResponseEntity<cartRequest> getCartById() {
+        String email = authUtil.loggedInEmail();
+        cartEntity cart = cartRepo.findCartByEmail(email);
+        Long cartId = cart.getCartId();
+        cartRequest cartRequest = cartService.getCart(email,cartId);
+
+        return new ResponseEntity<cartRequest>(cartRequest, HttpStatus.FOUND);
+    }
+
 }
