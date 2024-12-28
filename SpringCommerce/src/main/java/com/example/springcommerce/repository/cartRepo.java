1547e861426114243097b940f984141672365e2d
Ashish jha <ajha5645@gmail.com>
2024-12-29 00:23:53 +0530

    fixed minor bug for update cartItem quantity

--- SpringCommerce/src/main/java/com/example/springcommerce/repository/cartRepo.java
+++ SpringCommerce/src/main/java/com/example/springcommerce/repository/cartRepo.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface cartRepo extends JpaRepository<cartEntity, Long> {
 
@@ -13,4 +15,7 @@ public interface cartRepo extends JpaRepository<cartEntity, Long> {
 
     @Query("SELECT c FROM cartEntity c WHERE c.user.email = ?1 AND c.cartId = ?2")
     cartEntity findCartByEmailAndCartId(String email, Long cartId);
+
+    @Query("SELECT c FROM cartEntity c  JOIN FETCH c.cartItems ci JOIN FETCH ci.product p where p.productId = ?1")
+    List<cartEntity> findCartsByProductId(Long productId);
 }
