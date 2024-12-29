60e858eef3449ae26a5414a551d0fa4809a03cfc
Ashish jha <ajha5645@gmail.com>
2024-12-29 14:19:45 +0530

    this code will add the functionality to create teh address

--- SpringCommerce/src/main/java/com/example/springcommerce/serviceImplementation/addressServiceImpl.java
+++ SpringCommerce/src/main/java/com/example/springcommerce/serviceImplementation/addressServiceImpl.java
@@ -1,15 +1,39 @@
 package com.example.springcommerce.serviceImplementation;
 
 import com.example.springcommerce.DTO.Request.addressRequest;
+import com.example.springcommerce.entity.addressEntity;
 import com.example.springcommerce.entity.userEntity;
+import com.example.springcommerce.repository.addressRepo;
 import com.example.springcommerce.service.addressService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class addressServiceImpl implements addressService {
 
+    private final addressRepo addressRepository;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    addressServiceImpl(addressRepo addressRepository, ModelMapper modelMapper) {
+        this.addressRepository = addressRepository;
+        this.modelMapper = modelMapper;
+    }
+
     @Override
     public addressRequest createAddress(addressRequest addressRequest, userEntity user) {
-        return null;
+        addressEntity addressEntity = modelMapper.map(addressRequest, addressEntity.class);
+
+        List<addressEntity> addressList = user.getAddress();
+        addressList.add(addressEntity);
+        user.setAddress(addressList);
+
+        addressEntity.setUser(user);
+        addressEntity savedAddress = addressRepository.save(addressEntity);
+
+        return modelMapper.map(savedAddress, addressRequest.class);
     }
 }
