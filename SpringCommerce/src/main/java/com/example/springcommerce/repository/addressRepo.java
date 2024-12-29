bdf56bc339dd2c36909f15d6f4c603433c72fc57
Ashish jha <ajha5645@gmail.com>
2024-12-29 13:22:53 +0530

    added new addressRepo.java.java Interface

--- SpringCommerce/src/main/java/com/example/springcommerce/repository/addressRepo.java
+++ SpringCommerce/src/main/java/com/example/springcommerce/repository/addressRepo.java
@@ -3,5 +3,5 @@ package com.example.springcommerce.repository;
 import com.example.springcommerce.entity.addressEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class addressRepo extends JpaRepository<addressEntity, Long> {
+public interface addressRepo extends JpaRepository<addressEntity, Long> {
 }
