4e92e79eadaeec2538d66c553a353f72cc3e7d57
Ashish jha <ajha5645@gmail.com>
2024-12-30 01:07:16 +0530

    update address of user

--- SpringCommerce/src/main/java/com/example/springcommerce/serviceImplementation/addressServiceImpl.java
+++ SpringCommerce/src/main/java/com/example/springcommerce/serviceImplementation/addressServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.springcommerce.DTO.Request.addressRequest;
 import com.example.springcommerce.entity.addressEntity;
 import com.example.springcommerce.entity.userEntity;
 import com.example.springcommerce.repository.addressRepo;
+import com.example.springcommerce.repository.userRepo;
 import com.example.springcommerce.service.addressService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +17,14 @@ public class addressServiceImpl implements addressService {
 
     private final addressRepo addressRepository;
     private final ModelMapper modelMapper;
+    private final userRepo userRepository;
+
 
     @Autowired
-    public  addressServiceImpl(addressRepo addressRepository, ModelMapper modelMapper) {
+    public  addressServiceImpl(addressRepo addressRepository, ModelMapper modelMapper, userRepo userRepo) {
         this.addressRepository = addressRepository;
         this.modelMapper = modelMapper;
+        this.userRepository = userRepo;
     }
 
     @Override
@@ -56,7 +60,7 @@ public class addressServiceImpl implements addressService {
     }
 
     @Override
-    public addressRequest updateAddress(Long id, addressRequest addressRequest, userEntity user) {
+    public addressRequest updateAddress(Long id, addressRequest addressRequest) {
         addressEntity addressEntity = modelMapper.map(addressRequest, addressEntity.class);
 
         addressEntity savedAddress = addressRepository.findById(id).orElseThrow(() -> new RuntimeException("Address not found"));
@@ -68,6 +72,13 @@ public class addressServiceImpl implements addressService {
         savedAddress.setBuildingName(addressEntity.getBuildingName());
 
         addressEntity updatedAddress = addressRepository.save(savedAddress);
+
+        userEntity userEntity = savedAddress.getUser();
+        userEntity.getAddress().removeIf(address -> address.getAddressId().equals(id));
+        userEntity.getAddress().add(updatedAddress);
+
+        userRepository.save(userEntity);
+
         return modelMapper.map(updatedAddress, addressRequest.class);
     }
 
