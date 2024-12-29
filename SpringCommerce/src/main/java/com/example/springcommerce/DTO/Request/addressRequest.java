01c1a8428abc12182bb0b63169168f19ee522b15
Ashish jha <ajha5645@gmail.com>
2024-12-29 13:17:54 +0530

    added new addressRequest.java DTO

---
+++ SpringCommerce/src/main/java/com/example/springcommerce/DTO/Request/addressRequest.java
@@ -0,0 +1,18 @@
+package com.example.springcommerce.DTO.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class addressRequest {
+    private Long addressId;
+    private String street;
+    private String buildingName;
+    private String city;
+    private String state;
+    private String country;
+    private String pinCode;
+}
