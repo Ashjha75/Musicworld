625c68abe95df8c4351a58f1d04d2933549e8853
Ashish jha <ajha5645@gmail.com>
2024-12-30 21:31:39 +0530

    addes sevice and implemntation for order controller

--- SpringCommerce/src/main/java/com/example/springcommerce/serviceImplementation/orderServiceImpl.java
+++ SpringCommerce/src/main/java/com/example/springcommerce/serviceImplementation/orderServiceImpl.java
@@ -1,8 +1,16 @@
 package com.example.springcommerce.serviceImplementation;
 
+import com.example.springcommerce.DTO.Request.orderBodyRequest;
+import com.example.springcommerce.DTO.Request.orderRequest;
 import com.example.springcommerce.service.orderService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class orderServiceImpl implements orderService
+public class orderServiceImpl implements orderService{
+
+
+    @Override
+    public orderRequest placeOrder(String emailId, String paymentMethod, orderBodyRequest orderRequestBody) {
+        return null;
+    }
 }
