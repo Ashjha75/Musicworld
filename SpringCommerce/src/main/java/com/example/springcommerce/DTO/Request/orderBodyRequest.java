615ba9a9c9bfbc24b28200720a6890cda9509add
Ashish jha <ajha5645@gmail.com>
2024-12-30 16:05:30 +0530

    added orderBodyRequest.java

---
+++ SpringCommerce/src/main/java/com/example/springcommerce/DTO/Request/orderBodyRequest.java
@@ -0,0 +1,10 @@
+package com.example.springcommerce.DTO.Request;
+
+public class orderBodyRequest {
+    private Long addressId;
+    private String paymentMethod;
+    private String paymentGatewayPaymentStatus;
+    private String paymentGatewayPaymentId;
+    private String paymentGatewayResponseMessage;
+    private String paymentGatewayName;
+}
