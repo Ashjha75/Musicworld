17b7d17e8354fb3c57e64e831c48df7ac4d5d6ea
Ashish jha <ajha5645@gmail.com>
2024-12-30 21:38:43 +0530

    addes sevice and implemntation for order controller

--- SpringCommerce/src/main/java/com/example/springcommerce/serviceImplementation/orderServiceImpl.java
+++ SpringCommerce/src/main/java/com/example/springcommerce/serviceImplementation/orderServiceImpl.java
@@ -2,7 +2,10 @@ package com.example.springcommerce.serviceImplementation;
 
 import com.example.springcommerce.DTO.Request.orderBodyRequest;
 import com.example.springcommerce.DTO.Request.orderRequest;
+import com.example.springcommerce.entity.cartEntity;
+import com.example.springcommerce.exception.ResourceNotFound;
 import com.example.springcommerce.service.orderService;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +13,14 @@ public class orderServiceImpl implements orderService{
 
 
     @Override
+    @Transactional
     public orderRequest placeOrder(String emailId, String paymentMethod, orderBodyRequest orderRequestBody) {
+
+        cartEntity cart = cartRepository.findByEmailId(emailId);
+        if(cart == null){
+            throw new ResourceNotFound("Cart","email",emailId);
+        }
+
         return null;
     }
 }
